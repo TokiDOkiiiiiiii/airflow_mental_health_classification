b708711670cb303ac9e84ab714e183211b109c24
TokiDOkiiiiiiii <napatphong46@gmail.com>
2026-04-22 17:54:33 +0700

    feat: temporary solution for skip task in airflow

--- dags/src/workflow/step5.py
+++ dags/src/workflow/step5.py
@@ -2,3 +2,5 @@ def update_deployment(**ctx):
     from src.utility.update import update_deployment_endpoint
     if ctx["ti"].xcom_pull(key="deploy", task_ids="evaluate_and_promote"):
         update_deployment_endpoint()
+    else:
+        print("skipped")
