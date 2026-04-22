7469d4967c1e3c6581fac10361693260404a1331
TokiDOkiiiiiiii <napatphong46@gmail.com>
2026-04-22 17:57:36 +0700

    fix: mismatch label

--- dags/src/utility/testing.py
+++ dags/src/utility/testing.py
@@ -3,7 +3,7 @@ from sklearn.metrics import f1_score
 
 
 def score_model(model, test_df):
-    LABEL2ID = {"Normal": 0, "Anxiety": 1, "Depression": 2, "Suicidal": 3}
+    LABEL2ID = {"Normal": 0, "Negative": 1, "Very Negative": 2, "Suicidal": 3}}
     all_labels = (test_df["status"].map(LABEL2ID)).tolist()
     prob = model.predict(test_df)
     all_preds = np.argmax(prob, 1).tolist()
